a3af25f3f225b0d3b4905909904ae9dc08287ab6
Jeff Marks <jeff.t.marks@gmail.com>
2022-07-18 21:54:04 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    return Math.abs(42 - block);
+}
+
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    return Math.abs(startBlock - endBlock) * 264;
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+    const travelDistance = distanceTravelledInFeet(startBlock, endBlock);
+    if (travelDistance <= 400) {
+        return 0;
+    } else if (travelDistance > 400 && travelDistance <= 2000) {
+        return (travelDistance - 400) * 0.02;
+    } else if (travelDistance > 2000 && travelDistance < 2500) {
+        return 25.00;
+    } else if (travelDistance > 2500);
+        return 'cannot travel that far';
+    }
