3d5b458f4f3ae6a45f9a438d8a8b26a1ac2b3024
Jeff Marks <jeff.t.marks@gmail.com>
2022-07-19 10:17:45 -0400

    commit tree 6d3e73e190ec0300fe1272d4f8911340f5ac24bd

--- index.js
+++ index.js
@@ -1,23 +1,65 @@
-function distanceFromHqInBlocks(block) {
-    return Math.abs(42 - block);
+//Using absolute value with Math.abs
+// function distanceFromHqInBlocks(block) {
+//     return Math.abs(42 - block);
+// }
+
+//Using conditional if statement
+// function distanceFromHqInBlocks(someValue) {
+//     if(someValue < 42) {
+//         return 42 - someValue;
+//     } else {
+//         return someValue - 42;
+//     }
+// }
+
+//Using ternary statement
+function distanceFromHqInBlocks(someValue) {
+    return someValue < 42 ? 42 - someValue : someValue - 42;
 }
 
 function distanceFromHqInFeet(block) {
     return distanceFromHqInBlocks(block) * 264;
 }
 
-function distanceTravelledInFeet(startBlock, endBlock) {
-    return Math.abs(startBlock - endBlock) * 264;
+// function distanceTravelledInFeet(startBlock, endBlock) {
+//     return Math.abs(startBlock - endBlock) * 264;
+// }
+
+// const distanceTravelledInFeet = (startBlock, endBlock) => {
+//     return Math.abs(startBlock - endBlock) * 264;
+// }
+
+
+const distanceTravelledInFeet = (startBlock, endBlock) => {
+    if (endBlock > startBlock) {
+        return (endBlock - startBlock) * 264;
+    } else {
+        return (startBlock - endBlock) * 264;
+    }
 }
+
+// function calculatesFarePrice(startBlock, endBlock) {
+//     const travelDistance = distanceTravelledInFeet(startBlock, endBlock);
+//     if (travelDistance <= 400) {
+//         return 0;
+//     } else if (travelDistance > 400 && travelDistance <= 2000) {
+//         return (travelDistance - 400) * 0.02;
+//     } else if (travelDistance > 2000 && travelDistance <= 2500) {
+//         return 25.00;
+//     } else if (travelDistance > 2500) {
+//         return 'cannot travel that far';
+//     }
+// }
 
 function calculatesFarePrice(startBlock, endBlock) {
     const travelDistance = distanceTravelledInFeet(startBlock, endBlock);
     if (travelDistance <= 400) {
         return 0;
-    } else if (travelDistance > 400 && travelDistance <= 2000) {
+    } else if (travelDistance <= 2000) {
         return (travelDistance - 400) * 0.02;
-    } else if (travelDistance > 2000 && travelDistance < 2500) {
+    } else if (travelDistance <= 2500) {
         return 25.00;
-    } else if (travelDistance > 2500);
-        return 'cannot travel that far';
+    } else {
+        return 'cannot travel that far';   
     }
+}
